ed7795bb802a837a73622255bd8fbdcb274050f4
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-10-11 02:56:43 +0500

    Conditional rendering in react

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,38 @@ import Buttons from "./components/Button";
 import Input from "./components/input";
 
 const App = () => {
-
+ const [notificationNumber, setNotificationNumber] = useState(0);
 
  return (
-  <Card >
-   <p>Hello this paren component</p>
-   <Buttons isActive={false}/>
-  </Card>
+  <>
+   <Card>
+    <p>Hello this paren component</p>
+    <Buttons isActive={false} />
+   </Card>
+   {notificationNumber >= 1 ? (
+    <Card>
+     <p>
+      You have {notificationNumber} new notification
+      {notificationNumber === 1 ? "" : "s"}
+     </p>
+    </Card>
+   ) : (
+    <Card>No notification</Card>
+   )}
+  </>
  );
 };
 
 export default App;
 
 const Card = styled.div`
-padding: 20px 30px;
-background: rgb(192, 77, 0, 0.12);
-border-radius: 25px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width: 300px
-`
+ padding: 20px 30px;
+ background: rgb(192, 77, 0, 0.12);
+ border-radius: 25px;
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ width: 300px;
+ margin: 15px;
+`;
