8fca7e15cfa955fd56db3ef985c10b3cf4cd2b45
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-10-11 02:27:12 +0500

    useEffect hook

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Button = () => {
  const [count, setCount] = useState(0);
+ useEffect(() => { 
+  document.title = `You clicked ${count} times`
+ }, [count])
 
  return (
   <>
