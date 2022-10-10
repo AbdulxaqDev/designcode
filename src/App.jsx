67101d2e2fb1839e9a44c6a829f9a3cfe24e9c64
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-10-11 02:45:33 +0500

    react component props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Buttons from "./components/Button";
 import Input from "./components/input";
 
@@ -6,10 +7,22 @@ const App = () => {
 
 
  return (
-  <div>
-   <Buttons/>
-  </div>
+  <Card >
+   <p>Hello this paren component</p>
+   <Buttons isActive={false}/>
+  </Card>
  );
 };
 
 export default App;
+
+const Card = styled.div`
+padding: 20px 30px;
+background: rgb(192, 77, 0, 0.12);
+border-radius: 25px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 300px
+`
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
-const Button = () => {
+const Button = ({ isActive }) => {
  const myRef = useRef(null);
 
  useEffect(() => {
@@ -12,25 +12,28 @@ const Button = () => {
 
  return (
   <>
-   <button onClick={() => alert("You clicked")} ref={myRef}>
-    Click
-   </button>
+   <MyButton
+    isActive={isActive}
+    onClick={() => alert("You clicked")}
+    ref={myRef}
+    disabled={!isActive}
+   >
+    Click (child component)
+   </MyButton>
   </>
  );
 };
 
 export default Button;
 
-// const MyButton = styled.button`
-//  background: ${(props) =>
-//   props.disabled
-//    ? "grey"
-//    : "linear-gradient(91.4deg, #2fb8ff 0%, #9eecd9 100%)"};
-//  padding: 12px 0;
-//  width: 200px;
-//  border: none;
-//  border-radius: 30px;
-//  color: white;
-//  font-weight: bold;
-//  font-family: Segoe UI, sans-serif;
-// `;
+const MyButton = styled.button`
+ background: ${({isActive}) => isActive ? "linear-gradient(91.4deg, #2fb8ff 0%, #9eecd9 100%)" : "grey"};
+ padding: 12px 0;
+ width: 200px;
+ border: none;
+ border-radius: 30px;
+ color: white;
+ font-weight: bold;
+ font-family: Segoe UI, sans-serif;
+ margin: 0 auto;
+`;
